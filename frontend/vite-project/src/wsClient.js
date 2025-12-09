e77aa575f7afca804cc4aa0a3ca1a8472454c2f3
SalazarSK <lubos.janoscik@gmail.com>
2025-12-09 22:02:34 +0100

    FEAT: Conversation connecting with WS

--- frontend/vite-project/src/wsClient.js
+++ frontend/vite-project/src/wsClient.js
@@ -5,13 +5,28 @@ const WS_URL = import.meta.env.VITE_API_URL + "/ws-chat";
 
 export const connectWebSocket = (onConnected) => {
   const client = new Client({
-    webSocketFactory: () => new SockJS(WS_URL),
+    webSocketFactory: () =>
+      new SockJS(WS_URL, null, { transports: ["websocket", "xhr-streaming"] }),
+    reconnectDelay: 3000,
     debug: () => {},
-    reconnectDelay: 5000,
   });
 
   client.onConnect = () => {
+    console.log("WS CONNECTED");
     onConnected(client);
+  };
+
+  client.onStompError = (frame) => {
+    console.error("STOMP Error:", frame.headers["message"]);
+    console.error("Body:", frame.body);
+  };
+
+  client.onWebSocketClose = (event) => {
+    console.warn("WebSocket closed:", event.reason || event);
+  };
+
+  client.onWebSocketError = (err) => {
+    console.error("WebSocket error:", err);
   };
 
   client.activate();
@@ -20,5 +35,10 @@ export const connectWebSocket = (onConnected) => {
 
 export const disconnectWebSocket = async (client) => {
   if (!client) return;
-  await client.deactivate();
+  try {
+    await client.deactivate();
+    console.log(" WS disconnected");
+  } catch (e) {
+    console.error("Error disconnecting WS", e);
+  }
 };
